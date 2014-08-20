bf08e6d7213e3fd4b99dc197cbfb72425f1b23b8
Zuon <lordzuon@gmail.com>
2014-08-19 23:54:28 -0400

    add lines

--- source/processor.cpp
+++ source/processor.cpp
@@ -8,17 +8,23 @@ Processor::Processor(unsigned sampleRate, unsigned size, std::ostream& errorStre
 	_errorStream(errorStream),
 	_sampleRate(sampleRate),
 	_samples(size, 0.0f),
-	_queue(128)
+	_queue(128),
+	_beat(0),
+	_samplesAfterBeat(0),
+	_samplesPerBeat(0),
+	_beatsPerLoop(0),
+	_nextSamplesPerBeat(0),
+	_nextBeatsPerLoop(0)
 {
 	Op op;
 	op.mic.resize(size);
 	_queue.setAll(op);
 }
 
-void Processor::addObject(const std::string& name, Object object, int channel){
-	_objects[name]=object;
-	_queue.write()->type=Op::ACTIVATE;
-	_queue.write()->object=&_objects[name];
+void Processor::addSonic(const std::string& name, Sonic sonic, int channel){
+	_sonics[name]=sonic;
+	_queue.write()->type=Op::SONIC;
+	_queue.write()->sonic=&_sonics[name];
 	_queue.nextWrite();
 }
 
@@ -28,24 +34,116 @@ void Processor::processText(const std::string& line){
 	ss>>s;
 	if(s=="sonic"){
 		std::string name;
-		unsigned channel;
+		int channel;
 		if(!(ss>>name)||!(ss>>channel))
 			_errorStream<<"Must specify name and channel of sonic."<<std::endl;
 		Sonic sonic(_samples.data(), _sampleRate);
-		_objects[name].type=Object::SONIC;
-		_objects[name].sonic=sonic;
-		_queue.write()->type=Op::ACTIVATE;
-		_queue.write()->object=&_objects[name];
+		_sonics[name]=sonic;
+		_queue.write()->type=Op::SONIC;
+		_queue.write()->sonic=&_sonics[name];
 		_queue.write()->channel=channel;
 		_queue.nextWrite();
 	}
-	else{
-		std::vector<unsigned char> midi;
-		midi.push_back(0x90);
-		midi.push_back(60);
-		midi.push_back(64);
-		_queue.write()->type=Op::MIDI;
-		_queue.write()->midi=midi;
+	else if(s=="tempo"){
+		unsigned tempo;
+		ss>>tempo;
+		_queue.write()->type=Op::TEMPO;
+		_queue.write()->samplesPerBeat=_sampleRate*60/tempo;
+		_queue.nextWrite();
+	}
+	else if(s=="length"){
+		unsigned beatsPerLoop;
+		ss>>beatsPerLoop;
+		_queue.write()->type=Op::LENGTH;
+		_queue.write()->beatsPerLoop=beatsPerLoop;
+		_queue.nextWrite();
+	}
+	else if(s=="line"){
+		std::string name;
+		int channel;
+		if(!(ss>>name)||!(ss>>channel)){
+			_errorStream<<"Must specify name and channel of line."<<std::endl;
+			return;
+		}
+		if(channel<0||channel>=16){
+			_errorStream<<"Channel must be between 0 and 15."<<std::endl;
+			return;
+		}
+		Line line;
+		Line::Event event;
+		event.beat=0.0f;
+		event.message.push_back(channel);
+		event.message.push_back(0);
+		event.message.push_back(64);
+		float stride=1.0f;
+		int octave=5;
+		while(ss>>s){
+			if(s=="o"){
+				int o;
+				ss>>o;
+				octave+=o;
+				if(octave<0){
+					octave=0;
+					_errorStream<<"Ignoring too-low octave change."<<std::endl;
+				}
+				else if(octave>15){
+					octave=15;
+					_errorStream<<"Ignoring too-high octave change."<<std::endl;
+				}
+			}
+			else if(s=="t"){
+				ss>>stride;
+			}
+			else if(s.size()==1){
+				if(s=="."){
+					event.beat+=stride;
+					continue;
+				}
+				int note=-1;
+				switch(s[0]){
+					case 'z': note=0; break;
+					case 's': note=1; break;
+					case 'x': note=2; break;
+					case 'd': note=3; break;
+					case 'c': note=4; break;
+					case 'v': note=5; break;
+					case 'g': note=6; break;
+					case 'b': note=7; break;
+					case 'h': note=8; break;
+					case 'n': note=9; break;
+					case 'j': note=10; break;
+					case 'm': note=11; break;
+					default:
+						_errorStream<<"Ignoring invalid token "<<s<<"."<<std::endl;
+						break;
+				}
+				if(note>=0){
+					//note on
+					event.message[0]&=0x0f;
+					event.message[0]|=0x90;
+					//note number
+					event.message[1]=12*octave+note;
+					//note on line event
+					line.events.push_back(event);
+					//note off line event
+					event.beat+=stride;
+					event.message[0]&=0x0f;
+					event.message[0]|=0x80;
+					line.events.push_back(event);
+				}
+			}
+			else _errorStream<<"Ignoring invalid token "<<s<<"."<<std::endl;
+		}
+		_lines[name]=line;
+	}
+	else if(s=="activateLine"){
+		ss>>s;
+		if(!_lines.count(s)){
+			_errorStream<<"Line "<<s<<" does not exist."<<std::endl;
+			return;
+		}
+		_queue.write()->type=Op::LINE;
+		_queue.write()->line=&_lines[s];
 		_queue.nextWrite();
 	}
 }
@@ -64,35 +162,79 @@ void Processor::processMic(const float* samples){
 }
 
 void Processor::output(float* samples){
+	//update beat
+	if(_samplesPerBeat&&_beatsPerLoop){
+		_samplesAfterBeat+=_samples.size();
+		if(_samplesAfterBeat>=_samplesPerBeat){
+			_samplesAfterBeat-=_samplesPerBeat;
+			++_beat;
+			if(_beat>=_beatsPerLoop){
+				_beat-=_beatsPerLoop;
+				//update length
+				if(_nextBeatsPerLoop){
+					_beatsPerLoop=_nextBeatsPerLoop;
+					_nextBeatsPerLoop=0;
+				}
+				//update tempo
+				if(_nextSamplesPerBeat){
+					_samplesPerBeat=_nextSamplesPerBeat;
+					_nextSamplesPerBeat=0;
+				}
+				//reset lines
+				for(auto line: _activeLines) line->i=0;
+			}
+		}
+	}
+	//lines
+	float beat=_beat+1.0f*_samplesAfterBeat/_samplesPerBeat;
+	for(auto line: _activeLines){
+		while(line->i<line->events.size()&&beat>=line->events[line->i].beat){
+			Op op;
+			op.type=Op::MIDI;
+			op.midi=line->events[line->i].message;
+			processOp(op);
+			++line->i;
+		}
+	}
+	//reset sample
 	for(unsigned i=0; i<_samples.size(); ++i) _samples[i]=0.0f;
+	//process ops
 	while(_queue.read()){
 		processOp(*_queue.read());
 		_queue.nextRead();
 	}
-	for(auto i: _channelToObject){
-		switch(i.second->type){
-			case Object::SONIC:
-				i.second->sonic.evaluate(_samples.size());
-				break;
-			default: break;
-		}
-	}
+	//process sonics
+	for(auto i: _channelToSonic) i.second->evaluate(_samples.size());
+	//copy from working buffer to output buffer
 	for(unsigned i=0; i<_samples.size(); ++i) samples[i]=_samples[i];
 }
+
+Processor::Line::Line(): i(0) {}
 
 void Processor::processOp(const Op& op){
 	switch(op.type){
 		case Op::MIDI:{
 			unsigned channel=op.midi[0]&0x0f;
-			if(!_channelToObject.count(channel)) break;
-			_channelToObject[channel]->sonic.processMidi(op.midi);
+			if(!_channelToSonic.count(channel)) break;
+			_channelToSonic[channel]->processMidi(op.midi);
 			break;
 		}
 		case Op::MIC:
 			for(unsigned i=0; i<_samples.size(); ++i) _samples[i]+=op.mic[i];
 			break;
-		case Op::ACTIVATE:
-			_channelToObject[op.channel]=op.object;
+		case Op::SONIC:
+			_channelToSonic[op.channel]=op.sonic;
+			break;
+		case Op::TEMPO:
+			if(_samplesPerBeat) _nextSamplesPerBeat=op.samplesPerBeat;
+			else _samplesPerBeat=op.samplesPerBeat;
+			break;
+		case Op::LENGTH:
+			if(_beatsPerLoop) _nextBeatsPerLoop=op.beatsPerLoop;
+			else _beatsPerLoop=op.beatsPerLoop;
+			break;
+		case Op::LINE:
+			_activeLines.push_back(op.line);
 			break;
 		default: break;
 	}
--- source/processor.hpp
+++ source/processor.hpp
@@ -13,33 +13,52 @@ namespace dlal{
 
 class Processor{
 	public:
-		struct Object{
-			enum Type{ SONIC };
-			Type type;
-			Sonic sonic;
-		};
 		Processor(unsigned sampleRate, unsigned size, std::ostream& errorStream);
-		void addObject(const std::string& name, Object, int channel);
+		void addSonic(const std::string& name, Sonic, int channel);
 		void processText(const std::string&);
 		void processMidi(const std::vector<unsigned char>& midi);
 		void processMic(const float* samples);
 		void output(float*);
 	private:
+		struct Line{
+			Line();
+			struct Event{
+				float beat;
+				std::vector<unsigned char> message;
+			};
+			std::vector<Event> events;
+			unsigned i;
+		};
 		struct Op{
-			enum Type{ MIDI, MIC, ACTIVATE };
+			enum Type{ MIDI, MIC, SONIC, TEMPO, LENGTH, LINE };
 			Type type;
 			std::vector<unsigned char> midi;//MIDI
 			std::vector<float> mic;//MIC
-			Object* object;//ACTIVATE
-			unsigned channel;//ACTIVATE
+			union{
+				struct{
+					Sonic* sonic;//SONIC
+					unsigned channel;//SONIC
+				};
+				unsigned samplesPerBeat;//TEMPO
+				unsigned beatsPerLoop;//LENGTH
+				Line* line;//LINE
+			};
 		};
 		void processOp(const Op&);
 		std::ostream& _errorStream;
 		const unsigned _sampleRate;
 		std::vector<float> _samples;
 		dlal::Queue<Op> _queue;
-		std::map<std::string, Object> _objects;
-		std::map<int, Object*> _channelToObject;
+		unsigned _beat;
+		unsigned _samplesAfterBeat;
+		unsigned _samplesPerBeat;
+		unsigned _beatsPerLoop;
+		unsigned _nextSamplesPerBeat;
+		unsigned _nextBeatsPerLoop;
+		std::map<std::string, Sonic> _sonics;
+		std::map<int, Sonic*> _channelToSonic;
+		std::map<std::string, Line> _lines;
+		std::vector<Line*> _activeLines;
 };
 
 }//namespace dlal
