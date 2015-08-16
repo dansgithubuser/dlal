845479020dcc2e9d9829eabdc957e9faad2ade0a
Dan <dansonlinepresence@gmail.com>
2015-08-16 14:32:50 -0400

    move sfml to move common location, change sfml interface to softboard interface

---
+++ interfaces/softboard/src/main.cpp
@@ -0,0 +1,52 @@
+#include "softboard.hpp"
+#include "dyad.hpp"
+
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+#include <cstdlib>
+#include <sstream>
+#include <thread>
+#include <chrono>
+
+int main(int argc, char** argv){
+	if(argc!=3){
+		std::cerr<<"usage: Softboard ip port\n";
+		return EXIT_FAILURE;
+	}
+	int port;
+	std::stringstream ss;
+	ss<<argv[2];
+	ss>>port;
+	if(!dyadInit(std::string(argv[1]), port)) return EXIT_FAILURE;
+	Softboard softboard;
+	sf::RenderWindow window(sf::VideoMode(640, 480), "dlal softboard");
+	window.setKeyRepeatEnabled(false);
+	int result=EXIT_SUCCESS;
+	while(window.isOpen()){
+		sf::Event event;
+		while(window.pollEvent(event)){
+			switch(event.type){
+				case sf::Event::KeyPressed:
+				case sf::Event::KeyReleased:
+					if(!dyadWrite(softboard.processKey(
+						event.key.code,
+						event.type==sf::Event::KeyPressed
+					))){
+						result=EXIT_FAILURE;
+						window.close();
+					}
+					break;
+				case sf::Event::Closed:
+					window.close();
+					break;
+				default: break;
+			}
+			window.clear();
+			window.display();
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	}
+	if(!dyadFinish()) return EXIT_FAILURE;
+	return result;
+}
