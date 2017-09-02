c5cf84b03c10e32033b2db690f50d03b385c6db1
Dan <dansonlinepresence@gmail.com>
2017-09-02 19:11:30 -0400

    (midi editor) midi files -- clean up

--- deps/midi/midi.py
+++ deps/midi/midi.py
@@ -1,383 +1,260 @@
-help={}
-help["song"]="List of tracks. Track 1 contains events that apply to all tracks, the other tracks contain individual events."
-help["track"]="List of events."
-help["event"]="An event has the form [description, position, values...]. The possible descriptions are \"ticks\", \"tempo\", \"time\", \"key\", \"note\", and \"bar\"."
-help["ticks"]="This type of event has the form [\"ticks\", position, number of ticks per quarter note]."
-help["tempo"]="This type of event has the form [\"tempo\", position, number of microseconds per quarter note]."
-help["time"]="This type of event has the form [\"time\", position, top of time signature, bottom]."
-help["key"]="This type of event has the form [\"key\", position, number of sharps in key signature, major(0) or minor(1)]."
-help["note"]="This type of event has the form [\"note\", position, duration, channel, note number]."
-help["bar"]="This type of event has the form [\"bar\", position]."
-help["transpose"]="transpose(song, amount) transposes song in place up by number of semitones specified in amount."
-help["bar"]="bar(song) adds bars to song in place. Note positions are recalculated relative to the last bar."
-help["unbar"]="unbar(song) removes bars from song in place."
-help["playing"]="playing(song, time) returns a list, each element is a list of notes playing, or [\"rest\"] if none are playing. The element number in the higher list corresponds to the track number (ignoring track 1)."
-help["harmony"]="harmony(song) returns a list of harmonies. Each harmony is a list of what note is playing in each track (ignoring track 1) plus the frequency of the harmony."
-help["write"]="write(filename, song)"
+track_header_size=8
 
-trackheaderlength=8
-
-#Return [delta, length] where
-#delta is the delta ticks encoded starting from bytes[0] and
-#length is the length of the delta time in bytes.
-#Return -1 in case of error.
-def getdelta(bytes):
+def de_delta(bytes, i):
+	'''Return (delta, i) where
+delta is the delta ticks encoded starting from bytes[0] and
+i is incremented by the length of the delta time in bytes.'''
 	result=0
-	for i in range(4):
+	for i in range(i, i+4):
 		result<<=7
-		result+=ord(bytes[i])&0x7f
-		if not ord(bytes[i])&0x80:#The most significant bit signals when to stop.
-			break
-		elif i==3:#Delta time should not be more than 4 bytes long.
-			return -1
-	return [result,i+1]
+		result+=bytes[i]&0x7f
+		if not bytes[i]&0x80: break
+	else: raise Exception('delta too big')
+	return (result, i+1)
 
-#Return the bytes that specify a delta time equal to ticks.
-def delta(ticks):
-	mask=0x7f
+def se_delta(ticks):
+	'Return the bytes that specify a delta time equal to ticks.'
 	result=[]
-	while True:
-		byte=ticks&mask
+	for i in range(4):
+		byte=ticks&0x7f
 		ticks=ticks>>7
 		result=[byte]+result
-		if len(result)>4:
-				return -1
 		if ticks==0:
-			for i in range(len(result)-1):
-				result[i]|=0x80
-			for i in range(len(result)):
-				result[i]=chr(result[i])
+			for i in range(len(result)-1): result[i]|=0x80
 			return result
+	else: raise Exception('delta too big')
 
-#Return the midi commands in trackchunk in a list.
-#trackchunk is assumed to have come from a track produced by chunkitize.
-#Return -1 in case of error
-def getcommands(trackchunk):
-	commands=[]
-	global trackheaderlength
-	i=trackheaderlength
-	while i<len(trackchunk):
-		commands+=[[]]
-		result=getdelta(trackchunk[i:])
-		if result==-1:
-			return -1
-		commands[-1]+=[result[0]]
-		i+=result[1]
-		if ord(trackchunk[i])&0xf0 in [0x80,0x90,0xa0,0xb0,0xe0]:
-			commands[-1]+=[trackchunk[i:i+3]]
-			i+=3
-		elif ord(trackchunk[i])&0xf0 in [0xc0,0xd0]:
-			commands[-1]+=[trackchunk[i:i+2]]
+class Pair:
+	'The track unit: a delta-time and MIDI event pair.'
+	def __init__(self, delta, event):
+		self.delta=delta
+		self.event=event
+
+def get_pairs(track_chunk):
+	'''Return the pairs in track_chunk in a list.
+track_chunk is assumed to have come from a track chunk produced by chunkitize.'''
+	pairs=[]
+	i=track_header_size
+	command=None
+	while i<len(track_chunk):
+		delta, i=de_delta(track_chunk, i)
+		x=track_chunk[i]
+		if x&0xf0: command=x; i+=1
+		if command&0xf0 in [0x80,0x90,0xa0,0xb0,0xe0]:
+			parameters=track_chunk[i:i+2]
 			i+=2
-		elif ord(trackchunk[i])&0xf0==0xf0:
-			if ord(trackchunk[i])==0xff:
-				commands[-1]+=[trackchunk[i:i+3+ord(trackchunk[i+2])]]
-				i+=3+ord(trackchunk[i+2])
+		elif command&0xf0 in [0xc0,0xd0]:
+			parameters=track_chunk[i:i+1]
+			i+=1
+		elif command&0xf0==0xf0:
+			if command==0xff:
+				l=2+track_chunk[i+1]
+				parameters=track_chunk[i:i+l]
+				i+=l
 			else:
-				commands[-1]+=[trackchunk[i]]
-				i+=1
-		else:
-			return -1
-	if len(commands)<1:
-		return -1
-	if ord(commands[-1][1][1])!=0x2f:
-		return -1
-	return commands
+				parameters=[]
+		pairs.append(Pair(delta, [command]+parameters))
+	if pairs[-1].event!=[0xff, 0x2f, 0x00]: raise Exception('invalid last command')
+	return pairs
 
-#return an unsigned integer from a big endian string of bytes
-def uintb(bytes):
+#return an unsigned integer from a big-endian string of bytes
+def big_endian_to_unsigned(bytes):
 	result=0
 	for byte in bytes:
 		result<<=8
-		result+=ord(byte)
+		result+=byte
 	return result
 
-#return a list of lists of of midi file bytes, separated into the header and tracks
-#return -1 if the midi file doesn't look as expected
+#return a list of lists of MIDI file bytes, separated into the header and tracks
 def chunkitize(bytes):
-	headerlength=14
-	headertitle="MThd"
-	if len(bytes)<headerlength:
-		return -1
-	if bytes[0:len(headertitle)]!=headertitle:
-		return -1
-	chunks=[bytes[0:headerlength]]
-	i=headerlength
-	tracktitle="MTrk"
-	global trackheaderlength
+	header_length=14
+	header_title=[ord(i) for i in 'MThd']
+	if len(bytes)<header_length: raise Exception('too short')
+	if bytes[0:len(header_title)]!=header_title: raise(Exception('bad header'))
+	chunks=[bytes[0:header_length]]
+	track_title=[ord(i) for i in 'MTrk']
+	i=header_length
+	global track_header_size
 	while True:
-		if len(bytes)<i+trackheaderlength:
-			break
-		if bytes[i:i+len(tracktitle)]!=tracktitle:
-			return -1
-		tracklength=uintb(bytes[i+4:i+8])
-		if len(bytes)<i+trackheaderlength+tracklength:
-			return -1
-		chunks+=[bytes[i:i+trackheaderlength+tracklength]]
-		i+=trackheaderlength+tracklength
-	if i!=len(bytes):
-		return -1
-	if uintb(bytes[10:12])!=len(chunks)-1:
-		return -1
+		if len(bytes)<i+track_header_size: break
+		if bytes[i:i+len(track_title)]!=track_title: raise Exception('track too short')
+		tracklength=big_endian_to_unsigned(bytes[i+4:i+8])
+		if len(bytes)<i+track_header_size+tracklength: raise Exception('bad track header')
+		chunks+=[bytes[i:i+track_header_size+tracklength]]
+		i+=track_header_size+tracklength
+	if i!=len(bytes): raise Exception('malformed tracks')
+	if big_endian_to_unsigned(bytes[10:12])!=len(chunks)-1: raise Exception('bad size')
 	return chunks
 
-#Take the number of sharps in the key signature (negative value for flats)
-#and return the semitone number of the root, C being 0.
-def sharpstosemitones(sharps, minor):
-	return (sharps*7-minor*3)%12
+class Event:
+	@staticmethod
+	def make(type, ticks, *args):
+		r=Event()
+		r.type=type
+		r.ticks=ticks
+		if type=='ticks_per_quarter':
+			r.ticks_per_quarter=args[0]
+		elif type=='tempo':
+			r.us_per_quarter=args[0]
+		elif type=='time_sig':
+			r.top=args[0]
+			r.bottom=args[1]
+		elif type=='key_sig':
+			r.sharps=args[0]
+			r.minor=args[1]
+		elif type=='note':
+			r.duration=args[0]
+			r.channel=args[1]
+			r.number=args[2]
+		else: raise Exception('invalid type')
+		return r
 
-#Parse a midi bytes and return a song
-#Return -1 in case of error
+	def split_note(self):
+		assert self.type=='note'
+		on=Event()
+		on.type='note_on'
+		on.ticks=self.ticks
+		on.channel=self.channel
+		on.number=self.number
+		off=Event()
+		off.type='note_off'
+		off.ticks=self.ticks+self.duration
+		off.channel=self.channel
+		off.number=self.number
+		return [on, off]
+
+	def __lt__(self, other): return self.ticks<other.ticks
+
+	def __repr__(self):
+		attrs=dir(self)
+		attrs=[i for i in attrs if not i.startswith('_')]
+		attrs=[i for i in attrs if i!='type' and i!='ticks']
+		attrs=[i for i in attrs if not callable(getattr(self, i))]
+		attrs=['{}: {}'.format(i, getattr(self, i)) for i in attrs]
+		return '{}({}; {})'.format(self.type, self.ticks, ', '.join(attrs))
+
+#Parse MIDI bytes and return a song
 def parse(bytes):
 	chunks=chunkitize(bytes)
-	if chunks==-1:
-		return -1
-	ticksperquarter=uintb(chunks[0][12:14])
-	if ticksperquarter==0:
-		return -1
-	if uintb(chunks[0][8:10])==1:
-		song=[]
-		if len(chunks)<2:
-			return song
-		commands=getcommands(chunks[1])
+	ticks_per_quarter=big_endian_to_unsigned(chunks[0][12:14])
+	if ticks_per_quarter==0: raise Exception('invalid ticks per quarter')
+	if big_endian_to_unsigned(chunks[0][8:10])!=1: raise Exception('unhandled file type')
+	song=[]
+	if len(chunks)<2: return song
+	pairs=get_pairs(chunks[1])
+	ticks=0
+	track=[Event.make('ticks_per_quarter', ticks, ticks_per_quarter)]
+	for pair in pairs:
+		ticks+=pair.delta
+		if pair.event[0]&0xf0==0xf0:
+			if pair.event[0]==0xff:
+				if pair.event[1]==0x51:
+					track+=[Event.make('tempo', ticks, big_endian_to_unsigned(pair.event[3:6]))]
+				elif pair.event[1]==0x58:
+					track+=[Event.make('time_sig', ticks, pair.event[3], 1<<pair.event[4])]
+				elif pair.event[1]==0x59:
+					sharps=pair.event[3]
+					if sharps&0x80: sharps=(sharps&0x7f)-0x80
+					track+=[Event.make('key_sig', ticks, sharps, pair.event[4])]
+	song+=[track]
+	for i in range(2, len(chunks)):
 		ticks=0
-		track=[["ticks", ticks, ticksperquarter]]
-		for command in commands:
-			ticks+=command[0]
-			if ord(command[1][0])&0xf0==0xf0:
-				if ord(command[1][0])==0xff:
-					if ord(command[1][1])==0x51:#Tempo
-						track+=[["tempo", ticks, uintb(command[1][3:6])]]
-					elif ord(command[1][1])==0x58:#Time signature
-						track+=[["time", ticks, ord(command[1][3]), 1<<ord(command[1][4])]]
-					elif ord(command[1][1])==0x59:#Key signature
-						sharps=ord(command[1][3])
-						if sharps&0x80:
-							sharps=(sharps&0x7f)-0x80
-						track+=[["key", ticks, sharps, ord(command[1][4])]]
+		pairs=get_pairs(chunks[i])
+		track=[]
+		for i in range(len(pairs)):
+			pair=pairs[i]
+			ticks+=pair.delta
+			if pair.event[0]&0xf0==0x90 and pair.event[2]!=0:#Note on
+				duration=0
+				for j in pairs[i+1:]:
+					duration+=j.delta
+					if j.event[0]&0xf0==0x90 and j.event[2]==0 or j.event[0]&0xf0==0x80:#Note off
+						if pair.event[1]==j.event[1]: break
+				track+=[Event.make('note', ticks, duration, pair.event[0]&0x0f, pair.event[1])]
 		song+=[track]
-		for i in range(2,len(chunks)):
-			ticks=0
-			commands=getcommands(chunks[i])
-			track=[]
-			for i in range(len(commands)):
-				command=commands[i]
-				ticks+=command[0]
-				if ord(command[1][0])&0xf0==0x90 and ord(command[1][2])!=0:#Note on
-					duration=0
-					for offcommand in commands[i+1:]:
-						duration+=offcommand[0]
-						if ord(offcommand[1][0])&0xf0==0x90 and ord(offcommand[1][2])==0 or ord(offcommand[1][0])&0xf0==0x80:#Note off
-							if offcommand[1][1]==command[1][1]:
-								break
-					track+=[["note", ticks, duration, ord(command[1][0])&0x0f, ord(command[1][1])]]
-			song+=[track]
-		return song
-	return -1
+	return song
 
-#Read a midi file and return a nicely constructed list that represents the song in the midi.
-#Return -1 in case of error
+#Read a MIDI file and return a nicely constructed list that represents the song in the MIDI.
 #The list is structured as follows:
 #It is a list of tracks.
 #The first track specifies things that correspond to all tracks.
 #The other tracks specify things specific to themselves.
 #Each track is a list of events.
-#An event has the form [description, position, values...]
-#description is a string describing the type of event.
-#position is the position of the event in the song in ticks.
-#values is a bunch of values based on which kind of event it is.
-#Here's a list of description and their corresponding values
-#"ticks": Value is number of ticks per quarter note.
-#"tempo": Value is number of microseconds per quarter note.
-#"time": Values are top of time signature, bottom of time signature.
-#"key": Values are number of sharps (negative means flats), major (0) or minor (1)
-#"note": Values are duration, channel, note number
 def read(filename):
-	file=open(filename,"rb")#Open in binary read mode.
-	bytes=file.read()
-	file.close()
+	def to_int(x):
+		if type(x)==str: return ord(x)
+		return int(x)
+	with open(filename, 'rb') as file: bytes=[to_int(i) for i in file.read()]
 	return parse(bytes)
 
-#Write a midi track to a file based on a list of bytes.
-#The track header and end message are appended automatically, so they should not be included in bytes.
-def writetrack(file, bytes):
-	bytes=["\0","\xff","\x01","\0"]+bytes+["\x01","\xff","\x2f","\0"]#Put a 1 here to match Sibelius 2.
-	size0=len(bytes)%0x100
-	size1=(len(bytes)>>8)%0x100
-	size2=(len(bytes)>>16)%0x100
-	size3=(len(bytes)>>24)
-	trackheader=["M","T","r","k",chr(size3),chr(size2),chr(size1),chr(size0)]
-	bytes=trackheader+bytes
-	for byte in bytes:
-		file.write(byte)
-	return
+def to_big_endian(x, size):
+	return [(x>>((size-1-i)*8))&0xff for i in range(size)]
 
-def comparetime(message1, message2):
-	if message1[1]<message2[1]:
-		return -1
-	elif message1[1]==message2[1]:
-		return 0
-	else:
-		return 1
+#Write a MIDI track to a file based on a list of bytes.
+#The track header and end message are appended automatically, so they should not be included in bytes.
+def write_track(file, bytes):
+	bytes=[0, 0xff, 0x01, 0]+bytes+[1, 0xff, 0x2f, 0]#Prepend a text event to match Sibelius 2.
+	track_header=[ord(i) for i in 'MTrk']+to_big_endian(len(bytes), 4)
+	bytes=track_header+bytes
+	file.write(bytearray(bytes))
 
 def ilog2(x):
 	result=-1
 	while x!=0:
 		x>>=1
 		result+=1
+	if result==-1: raise Exception('input outside domain')
 	return result
 
-#Write a midi file based on a nicely contructed list.
-def write(filename, song):
-	file=open(filename,"wb")
-	ticks=0
-	for event in song[0]:
-		if event[0]=="ticks":
-			ticks=event[2]
-	if ticks==0:
-		return -1
+#Write a MIDI file based on a nicely constructed list.
+def write(file_name, song):
+	file=open(file_name, 'wb')
+	assert song[0][0].type=='ticks_per_quarter'
+	ticks_per_quarter=song[0][0].ticks_per_quarter
+	if ticks_per_quarter==0: raise Exception('no ticks per quarter')
 	tracks=len(song)
-	header=["M","T","h","d","\0","\0","\0","\6","\0","\1","\0",chr(tracks),chr(ticks>>8),chr(ticks&0xff)]
-	for byte in header:
-		file.write(byte)
+	header=[ord(i) for i in 'MThd']+[0, 0, 0, 6, 0, 1, 0, tracks]+to_big_endian(ticks_per_quarter, 2)
+	file.write(bytearray(header))
 	bytes=[]
-	lasttime=0
-	for event in song[0]:
-		if event[0]=="tempo":
-			bytes+=delta(event[1]-lasttime)
-			bytes+=["\xff","\x51","\x03"]
-			bytes+=[chr(event[2]>>16),chr((event[2]>>8)&0xff),chr(event[2]&0xff)]
-		elif event[0]=="time":
-			bytes+=delta(event[1]-lasttime)
-			bytes+=["\xff","\x58","\x04"]
-			bytes+=[chr(event[2]),chr(ilog2(event[3])),chr(24),chr(8)]
-		elif event[0]=="key":
-			bytes+=delta(event[1]-lasttime)
-			bytes+=["\xff","\x59","\x02"]
-			sharps=event[2]
-			if sharps<0:
-				sharps=0x100+sharps
-			bytes+=[chr(sharps),chr(event[3])]
-		lasttime=event[1]
-	writetrack(file, bytes)
+	last_time=0
+	for event in song[0][1:]:
+		bytes+=se_delta(event.ticks-last_time)
+		if event.type=='tempo':
+			bytes+=[0xff, 0x51, 0x03]
+			bytes+=to_big_endian(event.us_per_quarter, 3)
+		elif event.type=='time_sig':
+			bytes+=[0xff, 0x58, 0x04]
+			bytes+=[event.top, ilog2(event.bottom), 24, 8]
+		elif event.type=='key_sig':
+			bytes+=[0xff, 0x59, 0x02]
+			sharps=event.sharps
+			if sharps<0: sharps=0x100+sharps
+			bytes+=[sharps, event.minor]
+		else: raise Exception('unhandled event type: {}'.format(event.type))
+		last_time=event.ticks
+	write_track(file, bytes)
 	for track in song[1:]:
-		messages=[]
+		notes=[]
 		for event in track:
-			if event[0]=="note":
-				messages+=[["on",event[1],event[3],event[4]]]
-				messages+=[["off",event[1]+event[2],event[3],event[4]]]
-		messages.sort(comparetime)
-		lasttime=0
-		for i in range(len(messages)):
-			temp=messages[i][1]
-			messages[i][1]=messages[i][1]-lasttime
-			lasttime=temp
+			if event.type=='note': notes+=event.split_note()
+		notes.sort()
+		last_time=0
+		for i in range(len(notes)):
+			temp=notes[i].ticks
+			notes[i].ticks=notes[i].ticks-last_time
+			last_time=temp
 		bytes=[]
-		for message in messages:
-			if message[0]=="on":
-				bytes+=delta(message[1])
-				bytes+=[chr(0x90|message[2])]
-				bytes+=[chr(message[3])]
-				bytes+=["\x79"]
-			elif message[0]=="off":
-				bytes+=delta(message[1])
-				bytes+=[chr(0x80|message[2])]
-				bytes+=[chr(message[3])]
-				bytes+=["\0"]
-		writetrack(file, bytes)
+		for note in notes:
+			if note.type=='note_on':
+				bytes+=se_delta(note.ticks)
+				bytes+=[0x90|note.channel]
+				bytes+=[note.number]
+				bytes+=[0x79]
+			elif note.type=='note_off':
+				bytes+=se_delta(note.ticks)
+				bytes+=[0x80|note.channel]
+				bytes+=[note.number]
+				bytes+=[0]
+		write_track(file, bytes)
 	file.close()
 	return
-
-def transpose(song, amount):
-	for i in range(len(song)):
-		for j in range(len(song[i])):
-			if song[i][j][0]=="note":
-				song[i][j][4]+=amount
-			elif song[i][j][0]=="key":
-				song[i][j][2]+=amount
-	return
-
-def bar(song):
-	for event in song[0]:
-		if event[0]=="ticks":
-			ticks=event[2]
-		elif event[0]=="time":#make this better -- time sig could change
-			top=event[2]
-			bottom=event[3]
-	bar=4*ticks*top/bottom
-	for i in range(len(song)):
-		offset=bar
-		j=0
-		while j<len(song[i]):
-			while song[i][j][1]>=offset:
-				song[i].insert(j,["bar",offset])
-				offset+=bar
-				j+=1
-			song[i][j][1]%=bar
-			j+=1
-	return
-
-def unbar(song):
-	for i in range(len(song)):
-		bar=0
-		j=0
-		while j<len(song[i]):
-			if song[i][j][0]=="bar":
-				bar=song[i][j][1]
-				del song[i][j]
-			else:
-				song[i][j][1]+=bar
-				j+=1
-	return
-
-def playing(song, time):
-	result=[]
-	for track in song[1:]:
-		result+=[["rest"]]
-		for event in track:
-			if event[0]=="note":
-				if event[1]+event[2]>time:
-					if event[1]<=time:
-						if result[-1]==["rest"]:
-							result[-1]=[event[4]]
-						else:
-							result[-1]+=[event[4]]
-			if event[1]>time:
-				break
-	return result
-
-def harmony(song):
-	tracks=song[1:]
-	i=[0]*len(tracks)
-	result=[]
-	while True:
-		#find minimum time, doesn't have to be unique
-		ind=len(i)#poison value
-		min=-1
-		dur=0
-		for j in range(len(i)):
-			if i[j]<len(tracks[j]) and (tracks[j][i[j]][1]<min or min==-1):
-				min=tracks[j][i[j]][1]
-				dur=tracks[j][i[j]][2]
-				ind=j
-		if min==-1:
-			print "no minimum time found."
-		#find pairs of concurrent notes
-		import rel
-		temp=rel.cproduct(playing(song, tracks[ind][i[ind]][1]))
-		#Add to result
-		for x in temp:
-			if x in [y[0:2] for y in result]:
-				result[[y[0:2] for y in result].index(x)][2]+=1
-			else:
-				result+=[x+[1]]
-		#increment the track we just looked at so we don't count it again
-		i[ind]+=1
-		#see if we need to quit
-		quit=True
-		for j in range(len(i)):
-			if i[j]<len(tracks[j]):
-				quit=False
-		if quit:
-			break
-	return result
--- deps/midi/test.py
+++ deps/midi/test.py
@@ -2,8 +2,8 @@ import midi
 
 x=midi.read('trans.mid')
 midi.write('trans2.mid', x)
-with open('trans.mid') as a:
-	with open('trans2.mid') as b:
+with open('trans.mid', 'rb') as a:
+	with open('trans2.mid', 'rb') as b:
 		x=a.read()
 		y=b.read()
 		if x!=y:
