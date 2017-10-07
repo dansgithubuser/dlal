b1635fac747207d61f72000aca4dd99667156e88
Dan <dansonlinepresence@gmail.com>
2017-10-07 18:52:40 -0400

    (midi editor) back to treble clef to make more space

--- interfaces/dansmidieditor/src/view.py
+++ interfaces/dansmidieditor/src/view.py
@@ -3,8 +3,8 @@ home=os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(home, '..', '..', '..', 'deps', 'midi'))
 import midi
 
-staff=[7, 11, 14, 17, 21, 28, 31, 35, 38, 41]
-semitones_per_staff=48
+treble=[4, 7, 11, 14, 17]
+semitones_per_staff=24
 
 class View:
 	def __init__(self, margin=6, text_size=12):
@@ -12,12 +12,12 @@ class View:
 		self.text=''
 		self.margin=margin
 		self.text_size=text_size
-		self.staves=3.1
+		self.staves=3.9
 		self.ticks=5760
 
 	def load(self, path): self.midi=midi.read(path)
 
-	def staves_to_draw(self): return int(self.staves)+1
+	def staves_to_draw(self): return min(int(self.staves)+1, len(self.midi)-1)
 
 	def h_note(self):
 		return int(self.h_window/self.staves/semitones_per_staff)
@@ -30,7 +30,7 @@ class View:
 		return ticks*self.w_window/self.ticks
 
 	def calculate_octave(self, staff):
-		octave=3
+		octave=5
 		lo=60
 		hi=60
 		for i in self.midi[staff+1]:
@@ -40,17 +40,18 @@ class View:
 			hi=max(hi, i.number)
 		top=(octave+2)*12
 		if hi>top: octave+=(hi-top+11)/12
-		bottom=(octave-2)*12
+		bottom=octave*12
 		if lo<bottom: octave+=(lo-bottom)/12
 		return octave
 
 	def notate_octave(self, octave):
-		octave-=3
-		if octave==0: return ''
-		return '{}v{}'.format(1+7*abs(octave), 'a' if octave<0 else 'b')
+		octave-=5
+		if octave>= 1: return '{}va'.format(1+7*octave)
+		if octave<=-1: return '{}vb'.format(1-7*octave)
+		return ''
 
 	def draw_notes_debug(self, media, octaves):
-		for i in range(min(len(self.midi)-1, self.staves_to_draw())):
+		for i in range(self.staves_to_draw()):
 			for j in self.midi[i+1]:
 				if j.type!='note': print(j)
 				if j.ticks>=self.ticks: break
@@ -67,24 +68,24 @@ class View:
 		self.h_window=media.height()
 		#staves
 		for i in range(self.staves_to_draw()):
-			for j in staff:
+			for j in treble:
 				y=self.y_note(i, j)
 				media.line(xi=0, xf=self.w_window, y=y, h=0, color=(0, 64, 0))
 		media.draw_vertices()
 		#octaves
 		octaves=[]
-		for i in range(min(len(self.midi)-1, self.staves_to_draw())):
-			octave=self.calculate_octave(i)
-			octaves.append(octave)
+		for i in range(self.staves_to_draw()):
+			octaves.append(self.calculate_octave(i))
 			media.text(
-				self.notate_octave(octave),
+				self.notate_octave(octaves[i]),
 				x=self.margin,
-				y=self.y_note(i, 27),
-				h=self.h_note()*4,
-				color=(0, 255, 0),
+				y=self.y_note(i, treble[-1]+2),
+				h=self.h_note()*2,
+				color=(0, 128, 0),
+				middle_y=True,
 			)
 		#notes
-		for i in range(min(len(self.midi)-1, self.staves_to_draw())):
+		for i in range(self.staves_to_draw()):
 			for j in self.midi[i+1]:
 				if j.type!='note': print(j)
 				if j.ticks>=self.ticks: break
@@ -93,6 +94,7 @@ class View:
 					y=self.y_note(i, j.number, octaves[i]),
 					w=self.x_ticks(j.duration),
 					h=self.h_note(),
+					color=(0, 64, 64),
 					middle_y=True,
 				)
 		media.draw_vertices()
