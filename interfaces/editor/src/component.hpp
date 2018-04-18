dansgithubuser/dlal

commit 37981fd9bbf6360db6935b762756d1a366e6374b

editor load

interfaces/editor/src/editor.cpp
@@ -109,6 +109,13 @@ extern "C" {
 		;
 	}
 
+	void editor_load(const char* fileName){
+		std::map<std::string, CartesianPair<int>> vPairs, cPairs;
+		std::ifstream(fileName)>>vPairs>>cPairs;
+		for(auto& i: vPairs) fVariables [i.first].moveTo(i.second.x, i.second.y);
+		for(auto& i: cPairs) fComponents[i.first].moveTo(i.second.x, i.second.y);
+	}
+
 	int addables_width(){ return ADDABLES_WIDTH; }
 
 	void* addable_at(int x, int y){
@@ -159,7 +166,8 @@ extern "C" {
 	}
 
 	void component_new(const char* name, const char* type, int x, int y){
-		fComponents[name].set(name, type, x, y);
+		if(fComponents.count(name)) fComponents.at(name)._type=type;//component was previously loaded
+		else fComponents[name].set(name, type, x, y);
 	}
 
 	void component_label(const char* name, const char* label){

interfaces/editor/src/config.py
@@ -77,6 +77,7 @@ class Controls(AbstractControls):
 			'q': 'quit',
 			'h': 'help',
 			'w': 'write',
+			'e': 'edit',
 		}
 		self.messaging=False
 		self.cpp=cpp
@@ -153,6 +154,8 @@ class Controls(AbstractControls):
 	def command_push(self, command): self.cpp.editor_push(command)
 	def command_write(self, file_name):
 		self.cpp.editor_save(file_name+'.editor')
+	def command_edit(self, file_name):
+		self.cpp.editor_load(file_name+'.editor')
 
 	#callback
 	def on_input(self):

commit 3a8808ca6d6123647007c52c710f32fd7d3993fa

(editor) fix up loading
problem introduced in 37981fd

interfaces/editor/src/component.hpp
@@ -10,7 +10,7 @@
 #include <string>
 
 struct Component: public Object {
-	Component(){}
+	Component& name(std::string s){ _name=s; return *this; }
 
 	Component& set(std::string name, std::string type, int x, int y, int* dx=nullptr, int* dy=nullptr){
 		_name=name;

interfaces/editor/src/editor.cpp
@@ -112,8 +112,10 @@ extern "C" {
 	void editor_load(const char* fileName){
 		std::map<std::string, CartesianPair<int>> vPairs, cPairs;
 		std::ifstream(fileName)>>vPairs>>cPairs;
-		for(auto& i: vPairs) fVariables [i.first].moveTo(i.second.x, i.second.y);
-		for(auto& i: cPairs) fComponents[i.first].moveTo(i.second.x, i.second.y);
+		for(auto& i: vPairs)
+			fVariables [i.first].name(i.first).moveTo(i.second.x, i.second.y);
+		for(auto& i: cPairs)
+			fComponents[i.first].name(i.first).moveTo(i.second.x, i.second.y);
 	}
 
 	void editor_name(){
@@ -166,11 +168,10 @@ extern "C" {
 
 	void variable_set(const char* name, const char* value){
 		if(fVariables.count(name)) fVariables.at(name)._value=value;
-		else{
-			Variable v(name, value);
-			v.moveTo(400, SZ*fVariables.size());
-			fVariables[name]=v;
-		}
+		else fVariables[name]
+			.name(name)
+			.value(value)
+			.moveTo(400, SZ*fVariables.size());
 	}
 
 	void component_new(const char* name, const char* type, int x, int y){

interfaces/editor/src/variable.hpp
@@ -11,9 +11,8 @@
 #include <obvious.hpp>
 
 struct Variable: public Object {
-	Variable(){}
-	Variable(std::string name, std::string value):
-		_name(name), _value(value) {}
+	Variable& name(std::string s){ _name=s; return *this; }
+	Variable& value(std::string s){ _value=s; return *this; }
 
 	void draw(bool selected) const {
 		dans_sfml_wrapper_text_draw(mouseX(), mouseY(), SZ, text().c_str(), 0, 255, selected?255:0, 255);