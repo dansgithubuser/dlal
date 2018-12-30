dansgithubuser/dlal

commit 6d7100d56265f6c9d85cfa263c98e8f4b79857a0

regularize component construction

dlal/skeleton.py
@@ -1,5 +1,6 @@
 import atexit
 import ctypes
+import functools
 import inspect
 import json
 import os
@@ -301,3 +302,8 @@ def component_with_name(system, name):
 	return Component(component_type, component=component)
 
 def test(): _skeleton.dlalTest()
+
+component_types=sorted(os.listdir(os.path.join(root, 'components')))
+for i in component_types:
+	if not component_try_import(i):
+		globals()[i.capitalize()]=functools.partial(Component, i)

commit 90932b71feb022c4ccb220cb11d79992ff4cef43

fix import order problem introduced in 6d7100d

dlal/__init__.py
@@ -9,3 +9,5 @@ from .qweboard import *
 from .reticulated_liner import *
 from .skeleton import *
 from .sonic_controller import *
+
+regularize_component_constructors()

dlal/skeleton.py
@@ -309,7 +309,8 @@ def component_with_name(system, name):
 
 def test(): _skeleton.dlalTest()
 
-component_types=sorted(os.listdir(os.path.join(root, 'components')))
-for i in component_types:
-	if not component_try_import(i):
-		globals()[i.capitalize()]=functools.partial(Component, i)
+def regularize_component_constructors():
+	component_types=sorted(os.listdir(os.path.join(root, 'components')))
+	for i in component_types:
+		if not component_try_import(i):
+			globals()[i.capitalize()]=functools.partial(Component, i)