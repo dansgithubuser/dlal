dansgithubuser/dlal

commit 5e6c4a7cf3875dfdb4ead57cda7950096c0319de

persistent component names

skeleton/src/skeleton.cpp
@@ -73,6 +73,10 @@ void dlalDemolishSystem(void* system){
 	delete (dlal::System*)system;
 }
 
+void* dlalComponentWithName(void* system, const char* name){
+	return ((dlal::System*)system)->_nameToComponent.at(name);
+}
+
 char* dlalSetVariable(void* system, const char* name, const char* value){
 	if(std::string(name ).find('\n')!=std::string::npos)
 		return dlal::toCStr("error: name cannot contain newline");
@@ -120,14 +124,16 @@ void dlalTest(){
 	dlal::AtomicList<int>::test();
 }
 
+std::ostream& operator<<(std::ostream& ostream, const dlal::Component* component){
+	return ostream<<componentToStr(component);
+}
+
 namespace dlal{
 
 Component* toComponent(void* p){ return (Component*)p; }
 
 std::string componentToStr(const Component* component){
-	std::stringstream ss;
-	ss<<component;
-	return ss.str();
+	return component->_name;
 }
 
 char* toCStr(const std::string& s){
@@ -270,6 +276,7 @@ std::string System::add(Component& component, unsigned slot, bool queue){
 		_componentsToAdd[slot].push_back(&component);
 	}
 	else _components[slot].push_back(&component);
+	_nameToComponent[component._name]=&component;
 	_reportQueue.write("add "+componentToStr(&component)+" "+component.type());
 	return "";
 }

dlal/commander.py
@@ -1,4 +1,5 @@
 from .skeleton import *
+from .skeleton import _skeleton
 
 class Commander(Component):
 	@staticmethod
@@ -67,13 +68,16 @@ class Commander(Component):
 			f=self.library.dlalCommanderConnect
 		else:
 			f=self.library.dlalCommanderDisconnect
+		def componentify(arg, connectee):
+			if type(arg) in [str, unicode]:
+				return _skeleton.dlalComponentWithName(self.system(), arg)
+			return arg.output() if connectee else arg.component
 		for i in range(len(args)-1):
-			if type(args[i+0]) in [str, unicode]: connector=ctypes.c_void_p(int(args[i+0], 16))
-			else: connector=args[i+0].output()
-			if type(args[i+1]) in [str, unicode]: connectee=ctypes.c_void_p(int(args[i+1], 16))
-			else: connectee=args[i+1].component
 			result+=report(f(
-				self.component, connector, connectee, edges_to_wait
+				self.component,
+				componentify(args[i  ], True ),
+				componentify(args[i+1], False),
+				edges_to_wait
 			))
 			if len(result): result+='\n'
 		return result

dlal/skeleton.py
@@ -30,12 +30,28 @@ def connect(*args):
 		if len(result): result+='\n'
 	return result
 
+class Namer:
+	def __init__(self): self.number=0
+
+	def register_name(self, name):
+		try:
+			number=int(name[1:])
+			self.number=max(number, self.number)
+		except: pass
+
+	def name(self):
+		self.number+=1
+		return 'c{}'.format(self.number)
+
+_namer=Namer()
+
 _skeleton=obvious.load_lib('Skeleton')
 obvious.set_ffi_types(_skeleton.dlalDemolishComponent, None, ctypes.c_void_p)
 obvious.set_ffi_types(_skeleton.dlalDyadInit)
 obvious.set_ffi_types(_skeleton.dlalDyadShutdown)
 obvious.set_ffi_types(_skeleton.dlalBuildSystem, ctypes.c_void_p, int)
 obvious.set_ffi_types(_skeleton.dlalDemolishSystem, None, ctypes.c_void_p)
+obvious.set_ffi_types(_skeleton.dlalComponentWithName, ctypes.c_void_p, ctypes.c_void_p, str)
 obvious.set_ffi_types(_skeleton.dlalSetVariable, ctypes.c_void_p, ctypes.c_void_p, str, str)
 obvious.set_ffi_types(_skeleton.dlalCommand, ctypes.c_void_p, ctypes.c_void_p, str)
 obvious.set_ffi_types(_skeleton.dlalAdd, ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p, ctypes.c_uint)
@@ -72,7 +88,7 @@ class System:
 		for arg in args:
 			for c in arg.components_to_add:
 				result+=report(_skeleton.dlalAdd(self.system, c.component, slot))
-			if len(result): result+='\n';
+			if len(result): result+='\n'
 		return result
 
 	def set(self, name, value):
@@ -90,7 +106,7 @@ class System:
 		#components
 		components={}
 		for name, serialized in state['components'].items():
-			component=Component(state['component_types'][name])
+			component=Component(state['component_types'][name], name=name)
 			component.deserialize(serialized)
 			components[name]=component
 		for slot in state['component_order']:
@@ -124,9 +140,11 @@ class Component:
 		if component_type not in Component._libraries:
 			Component._libraries[component_type]=obvious.load_lib(component_type.capitalize())
 			Component._libraries[component_type].dlalBuildComponent.restype=ctypes.c_void_p
+			Component._libraries[component_type].dlalBuildComponent.argtypes=[ctypes.c_char_p]
 		self.library=Component._libraries[component_type]
+		name=kwargs.get('name', _namer.name())
 		self.component=kwargs.get('component',
-			Component._libraries[component_type].dlalBuildComponent()
+			Component._libraries[component_type].dlalBuildComponent(name)
 		)
 		self.components_to_add=[self]
 		commands=[i.split()[0] for i in self.command('help').split('\n')[1:] if len(i)]

skeleton/src/skeleton.hpp
@@ -24,12 +24,16 @@
 #endif
 
 #define DLAL_BUILD_COMPONENT_DEFINITION(COMPONENT)\
-	void* dlalBuildComponent(){ return (dlal::Component*)new dlal::COMPONENT; }
+	void* dlalBuildComponent(const char* name){\
+		auto component=new dlal::COMPONENT;\
+		component->_name=name;\
+		return (dlal::Component*)component;\
+	}
 
 extern "C"{
 	//each component implements this
 	//return a new instance casted to dlal::Component*
-	DLAL void* dlalBuildComponent();
+	DLAL void* dlalBuildComponent(const char* name);
 
 	//implemented by skeleton
 	DLAL void dlalDemolishComponent(void* component);
@@ -37,6 +41,7 @@ extern "C"{
 	DLAL void dlalDyadShutdown();
 	DLAL void* dlalBuildSystem(int port);
 	DLAL void dlalDemolishSystem(void* system);
+	DLAL void* dlalComponentWithName(void* system, const char* name);
 	DLAL char* dlalSetVariable(void* system, const char* name, const char* value);
 	DLAL char* dlalCommand(void* component, const char* command);
 	DLAL char* dlalAdd(void* system, void* component, unsigned slot);
@@ -81,6 +86,7 @@ class System{
 		void evaluate();
 		std::string set(unsigned sampleRate, unsigned samplesPerEvaluation);
 		std::string serialize() const;
+		Component* componentWithName(const char* name);
 
 		dyad_Stream* dyadNewStream();
 		void dyadAddListener(dyad_Stream*, int event, dyad_Callback, void* userData);
@@ -93,6 +99,7 @@ class System{
 		std::vector<std::pair<std::string, std::string>> _reportConnections;
 		std::map<std::string, std::string> _variables;
 		std::vector<std::vector<Component*>> _components;
+		std::map<std::string, Component*> _nameToComponent;
 		dyad_Stream* _server;
 
 	private:
@@ -129,6 +136,7 @@ class Component{
 		virtual bool hasAudio(){ return false; }
 
 		System* _system;
+		std::string _name;
 		std::string _label;
 	protected:
 		typedef std::function<std::string(std::stringstream&)> Command;

commit d42e784f513a5f0b16d3f61ae1f07a8038abe2e7

fix segfault introduced in 5e6c4a7
surfaced when running `python go.py -c looper`
I thought I'd run that at some point between 5e6c4a7 and now...

overloading operator<< changed meaning here

some noteworthy downsides to overloading operators:
	- vanilla meaning goes away
	- hard to search uses
this bug was a result of these two things acting together

skeleton/src/skeleton.cpp
@@ -536,7 +536,7 @@ SamplesPerEvaluationGetter::SamplesPerEvaluationGetter(): _samplesPerEvaluation(
 Periodic::Periodic(): _period(0), _phase(0), _last(0.0f) {
 	registerCommand("periodic", "", [this](std::stringstream& ss){
 		std::stringstream tt;
-		tt<<(Periodic*)this;
+		tt<<(void*)(Periodic*)this;
 		return tt.str();
 	});
 	registerCommand("periodic_resize", "<period in samples>", [this](std::stringstream& ss){