4b297d3b54523cb71078389c10f3c7dd3e043508
Dan <dansonlinepresence@gmail.com>
2019-02-20 19:45:11 -0500

    (lpf) fix test

--- tests/lpf/test.py
+++ tests/lpf/test.py
@@ -5,5 +5,5 @@ import dlal
 sonic_controller=dlal.SonicController()
 lpf=dlal.Component('lpf')
 dlal.SimpleSystem.log_2_samples_per_evaluation=6
-system=dlal.SimpleSystem([sonic_controller, lpf], test=True)
+system=dlal.SimpleSystem([sonic_controller, lpf], [sonic_controller], test=True)
 go, ports=system.standard_system_functionality()
