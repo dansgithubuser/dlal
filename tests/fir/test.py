2a8235f6c30084a71e3d5341ceedf1a06d656cf9
Dan <dansonlinepresence@gmail.com>
2019-05-04 23:27:55 -0400

    fix fir test

--- tests/fir/test.py
+++ tests/fir/test.py
@@ -8,8 +8,8 @@ sonic_controller.s(0, 1, immediate=True)
 
 fir = dlal.Formant()
 fir.resize(128, immediate=True)
-fir.phonetic_voice('q')
 
 dlal.SimpleSystem.log_2_samples_per_evaluation = 6
 system = dlal.SimpleSystem([sonic_controller, fir], test=True)
+fir.phonetic_voice('q')
 go, ports = system.standard_system_functionality()
