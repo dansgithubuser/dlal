8c75cfdc1b337dfba12b5223409e909dd0f67ae1
Dan <dansonlinepresence@gmail.com>
2019-05-19 23:24:09 -0400

    repair comb and dalek systems

--- systems/comb.py
+++ systems/comb.py
@@ -5,10 +5,11 @@ multiplier = dlal.Component('multiplier')
 
 system = dlal.SimpleSystem([delay, multiplier], outputs=[delay])
 
-system.audio.connect(delay)
-multiplier.connect(delay)
+with dlal.ImmediateMode() as mode:
+    system.audio.connect(delay)
+    multiplier.connect(delay)
 
-delay.periodic_resize(dlal.round_up(system.sample_rate//100, system.samples_per_evaluation))
-multiplier.set('0.85')
+    delay.periodic_resize(dlal.round_up(system.sample_rate//100, system.samples_per_evaluation))
+    multiplier.set('0.85')
 
 go, ports = system.standard_system_functionality()
