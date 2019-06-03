c48cba3817206832e0a4102dfe6974ec687dbcc9
Dan <dansonlinepresence@gmail.com>
2019-06-02 21:11:29 -0400

    (web) refactor

---
+++ web/common.js
@@ -0,0 +1,42 @@
+var gSocket;
+var gPromiseResolvers = {};
+
+function e(name){ return document.getElementById(name) }
+function v(name){ return e(name).value }
+
+// Math.random is probably good enough for this...
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = Math.floor(Math.random() * 16), v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+function socketConnect(host, port, onOpen){
+  gSocket = new WebSocket(`ws:${host}:${port}`);
+  if (onOpen) gSocket.onopen = onOpen;
+  gSocket.onmessage = (event) => {
+    response = JSON.parse(event.data);
+    gPromiseResolvers[response.uuid].resolve(response);
+  };
+}
+
+function socketSend(path, options = {}){
+  if (!gSocket) return;
+  const uuid = options.uuid || uuidv4();
+  gSocket.send(JSON.stringify({
+    uuid,
+    path: path.split('.'),
+    args: options.args,
+    kwargs: options.kwargs,
+    op: options.op,
+  }));
+  return new Promise(function(resolve, reject){
+    gPromiseResolvers[uuid] = { resolve, reject };
+  });
+}
+
+function socketFree(uuid){
+  return socketSend('free', { op: 'store', uuid });
+}
+
