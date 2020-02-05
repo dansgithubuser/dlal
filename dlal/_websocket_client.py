9b2284b22c26c9f6c9f751f756f8bed77354a92e
Dan <dansonlinepresence@gmail.com>
2020-02-04 22:37:26 -0500

    don't hide exceptions in websocket client

--- dlal/_websocket_client.py
+++ dlal/_websocket_client.py
@@ -3,6 +3,7 @@ from ._system_server import SystemServer
 import websocket
 
 import threading
+import traceback
 import weakref
 
 class Forwarder:
@@ -10,8 +11,12 @@ class Forwarder:
         self.system_server = SystemServer(system)
         proxy = weakref.proxy(self)
         def on_message(ws, message):
-            response = proxy.system_server.handle_request(message)
-            ws.send(response)
+            try:
+                response = proxy.system_server.handle_request(message)
+                ws.send(response)
+            except Exception:
+                traceback.print_exc()
+                raise
         self.app = websocket.WebSocketApp(url, on_message=on_message)
         self.thread = threading.Thread(target=_run, args=(weakref.proxy(self.app),))
         self.thread.daemon = True
