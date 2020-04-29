17a44e0de715388ee1d3dbd6995f8e47ebfab48e
Dan <dansonlinepresence@gmail.com>
2020-04-28 21:31:38 -0400

    fleshing out S~ubsystem; simple Bpf

--- skeleton/dlal/_subsystem.py
+++ skeleton/dlal/_subsystem.py
@@ -2,16 +2,41 @@ from ._skeleton import connect as _connect
 
 class Subsystem:
     def __init__(self, name, components, inputs=[], outputs=[]):
-        from ._skeleton import component_class
         self.name = name
         self.components = {}
         for name, (kind, args, kwargs) in components.items():
-            kwargs['name'] = self.name + kwargs.get('name', name)
-            component = component_class(kind)(*args, **kwargs)
-            self.components[name] = component
-            setattr(self, name, component)
+            self.add(name, kind, args, kwargs)
         self.inputs = [self.components[i] for i in inputs]
         self.outputs = [self.components[i] for i in outputs]
+
+    def add(self, name, kind=None, args=[], kwargs={}):
+        from ._skeleton import component_class
+        if kind == None:
+            kind = name
+        kwargs['name'] = self.name + kwargs.get('name', name)
+        component = component_class(kind)(*args, **kwargs)
+        self.components[name] = component
+        setattr(self, name, component)
+
+    def add_to(self, driver):
+        for i in self.components.values():
+            driver.add(i)
+
+    def connect_inputs(self, other):
+        for i in self.inputs:
+            other.connect(i)
+
+    def connect_outputs(self, other):
+        for i in self.outputs:
+            i.connect(other)
+
+    def disconnect_inputs(self, other):
+        for i in self.inputs:
+            other.disconnect(i)
+
+    def disconnect_outputs(self, other):
+        for i in self.outputs:
+            i.disconnect(other)
 
 class FrequencyResponseAnalyzer(Subsystem):
     def __init__(self, name='fr', duration=10, sample_rate=44100, smoothness=200):
@@ -69,3 +94,18 @@ class FrequencyResponseAnalyzer(Subsystem):
             s /= (2 * self.smoothness + 1)
             smoothed.append((fr[i+self.smoothness][0], s))
         return smoothed
+
+class Bpf(Subsystem):
+    def __init__(self, order=1, name='bpf'):
+        components = {}
+        for i in range(1, order+1):
+            components[f'lpf{i}'] = ('lpf', [], {})
+            components[f'hpf{i}'] = ('hpf', [], {})
+        Subsystem.__init__(self, name,
+            components, components.keys(), components.keys())
+
+    def freq(self, freq, sample_rate=None):
+        args = [freq]
+        if sample_rate: args.append(sample_rate)
+        for i in self.components.values():
+            i.freq(*args)
