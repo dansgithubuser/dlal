b717191d04ec1e61f6c9ddadd44ec841bb57830b
Dan <dansonlinepresence@gmail.com>
2020-07-28 20:35:57 -0400

    (iir.py) remove frequency_response in favor of scipy/skeleton

--- skeleton/dlal/iir.py
+++ skeleton/dlal/iir.py
@@ -15,21 +15,3 @@ class Iir(Component):
 
     def b(self, b):
         return self.command('b', [b], do_json_prep=False)
-
-    def frequency_response(b=[1], a=[], steps=1024, sample_rate=44100):
-        a = [1] + a
-        result = []
-        for i in range(steps):
-            f = 10 ** (math.log10(sample_rate/2) * i / (steps-1))
-            w = 2 * math.pi * f / sample_rate
-            z = math.e ** (1j*w)
-            h = (
-                sum(b_i * z ** -i for i, b_i in enumerate(b))
-                /
-                sum(a_i * z ** -i for i, a_i in enumerate(a))
-            )
-            result.append((
-                f,
-                20 * math.log10(abs(h)),
-            ))
-        return result
