d7c8fdfe097e9f3cad3b517a10e151774a083293
Dan <dansonlinepresence@gmail.com>
2020-08-03 17:58:17 -0400

    iir web

--- skeleton/dlal/iir.py
+++ skeleton/dlal/iir.py
@@ -15,3 +15,13 @@ class Iir(Component):
 
     def b(self, b):
         return self.command('b', [b], do_json_prep=False)
+
+    def pole_zero(self, p, z, k=1):
+        from scipy import signal
+        if len(p) and type(p[0]) == dict:
+            p = [i['re'] + 1j * i['im'] for i in p]
+        if len(z) and type(z[0]) == dict:
+            z = [i['re'] + 1j * i['im'] for i in z]
+        b, a = signal.zpk2tf(z, p, k)
+        self.command_detach('a', [list(a)], do_json_prep=False)
+        self.command_detach('b', [list(b)], do_json_prep=False)
