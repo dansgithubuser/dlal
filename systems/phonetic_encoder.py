def2414ebd583f7e79ca9e602fd2b0d74af69ace
Dan <dansonlinepresence@gmail.com>
2020-09-28 18:35:33 -0400

    phonetic encoder stores formant freq and peak

--- systems/phonetic_encoder.py
+++ systems/phonetic_encoder.py
@@ -1,19 +1,12 @@
 #===== imports =====#
 import dlal
 
-import numpy as np
 from numpy.fft import fft
-from scipy import signal
 
 import argparse
-import cmath
-import copy
 import json
 import math
 import os
-import pdb
-import random
-import re
 
 try:
     import dansplotcore as dpc
@@ -29,9 +22,7 @@ parser.add_argument('--phonetics-file-path', default='assets/phonetics/phonetics
 parser.add_argument('--only', nargs='+')
 parser.add_argument('--start-from')
 parser.add_argument('--order', type=int, default=5)
-parser.add_argument('--plot-spectra', action='store_true')
 parser.add_argument('--plot-stop-ranges', action='store_true')
-parser.add_argument('--mutate-interactive', action='store_true')
 args = parser.parse_args()
 
 #===== consts =====#
@@ -98,134 +89,6 @@ def stop_ranges(x):
     # done
     return result
 
-def json_complex(re, im):
-    return {'re': re, 'im': im}
-
-class Filter:
-    def __init__(self, p, z, k, n, envelope=None):
-        self.p = p
-        self.z = z
-        self.k = k
-        self.n = n
-        self.envelope = envelope
-
-    def all_p(self):
-        return self.p + [i.conjugate() for i in self.p]
-
-    def all_z(self):
-        return self.z + [i.conjugate() for i in self.z]
-
-    def calc_error(self):
-        w, h = self.spectrum()
-        return sum(
-            (abs(h_i) - envelope_i) ** 2
-            for h_i, envelope_i
-            in zip(h, self.envelope)
-        )
-
-    def mutate(self, heat, max_pole_abs):
-        # individual mutations
-        def mutate_pole(i):
-            ret = i * abs(i) ** random.uniform(-0.5, 0.5)
-            if abs(ret) >= max_pole_abs:
-                ret = i
-            return ret
-        def mutate_zero(i):
-            ret = i * abs(i) ** random.uniform(-0.5, 0.5)
-            if abs(ret) >= 1:
-                ret = 1
-            return ret
-        # total mutations
-        r = random.randint(0, 100)
-        if r < 40:
-            # move each pole toward unit circle or origin, increase or decrease gain
-            k = self.k * random.uniform(0.5, 2) ** heat
-            if k == 0:
-                k = self.k
-            return Filter(
-                [mutate_pole(i) for i in self.p],
-                self.z,
-                k,
-                self.n,
-                self.envelope,
-            )
-        elif r < 80:
-            # move each zero toward unit circle or origin
-            return Filter(
-                self.p,
-                [mutate_zero(i) for i in self.z],
-                self.k,
-                self.n,
-                self.envelope,
-            )
-        else:
-            # alter frequency of a zero
-            i = random.randint(0, len(self.z)-1)
-            phase = cmath.phase(self.z[i])
-            if random.randint(0, 1):
-                if i == len(self.z)-1:
-                    p = math.pi
-                else:
-                    p = cmath.phase(self.p[i+1])
-            else:
-                p = cmath.phase(self.p[i])
-            t = random.uniform(0, heat)
-            phase = phase * (1-t) + p * t
-            z = copy.copy(self.z)
-            z[i] = cmath.rect(abs(z[i]), phase)
-            return Filter(self.p, z, self.k, self.n, self.envelope)
-
-    def tf(self):
-        return signal.zpk2tf(self.all_z(), self.all_p(), self.k)
-
-    def spectrum(self):
-        b, a = self.tf()
-        return signal.freqz(b, a, self.n//2+1, include_nyquist=True)
-
-    def h_max(self):
-        w, h = self.spectrum()
-        return max(float(abs(i)) for i in h)
-
-    def energy(self):
-        w, h = self.spectrum()
-        return sum((abs(i)/len(h))**2 for i in h)
-
-    def plot(self, log=False, bottom=None, scale=None, plot=None):
-        w, h = self.spectrum()
-        if plot == None:
-            plot = dpc.Plot(primitive=dpc.primitives.Line())
-        if log:
-            f = lambda x: 20 * math.log10(x)
-        else:
-            f = lambda x: x
-        y = [f(float(abs(i))) for i in h]
-        if scale:
-            y_max = max(y)
-            y = [i/y_max*scale for i in y]
-        if bottom:
-            y = [max(bottom, i) for i in y]
-        def transformed_line(xi, yi, xf, yf, r, g, b):
-            i = plot.transform(xi, yi, 0, plot.series)
-            f = plot.transform(xf, yf, 0, plot.series)
-            plot.line(i['x'], i['y'], f['x'], f['y'], r, g, b)
-        for i in self.p:
-            x = cmath.phase(i) / (2*math.pi) * self.n
-            transformed_line(x, 0, x, abs(i), 0, 255, 0)
-            transformed_line(x-10, abs(i)-0.1, x, abs(i), 0, 255, 0)
-        for i in self.z:
-            x = cmath.phase(i) / (2*math.pi) * self.n
-            transformed_line(x, 0, x, abs(i), 255, 0, 0)
-            transformed_line(x+10, abs(i)-0.1, x, abs(i), 255, 0, 0)
-        plot.plot(y)
-        if self.envelope:
-            y = [f(i) for i in self.envelope]
-            if scale:
-                y_max = max(y)
-                y = [scale*i/y_max for i in y]
-            x = [(self.n//2+1)*i/len(self.envelope) for i in range(len(self.envelope))]
-            plot.plot(x, y)
-        return plot
-
 def calc_n(x):
     n = 4096
     while n > len(x):
@@ -246,7 +109,7 @@ def calc_envelope(x, n):
         amp = 0
         for j in range(j_i, j_f):
             amp = max(amp, float(abs(spectrum[j])))
-        envelope.append(amp)
+        envelope.append(amp / n)
     return envelope
 
 def parameterize(x):
@@ -269,7 +132,7 @@ def parameterize(x):
         peak_f = peak_i
         while peak_f+1 < len(envelope) and envelope[peak_f+1].amp == peak_amp:
             peak_f += 1
-        freq = (peak_i + peak_f) / 2 / n * 2 * math.pi
+        freq = (peak_i + peak_f) / 2 * SAMPLE_RATE
         formants[freq] = peak_amp
         # visit right
         i = peak_i
@@ -285,49 +148,6 @@ def parameterize(x):
             if envelope[i].amp < envelope[i-1].amp:
                 break
             i -= 1
-    #----- gradient descent poles to match spectrum -----#
-    STEPS = 20
-    HEAT = 1
-    ANNEALING_MULTIPLIER = 1
-    BRANCHES = 320
-    MAX_POLE_ABS = 0.99
-    # init
-    p = sorted([
-        cmath.rect(min(0.9 * MAX_POLE_ABS, 1 - 1 / max(amp, 2)), freq)
-        for freq, amp in formants.items()
-    ], key=cmath.phase)
-    if any(i.imag < 0 for i in p) or any(abs(i) > MAX_POLE_ABS for i in p):
-        raise Exception('improper initial pole')
-    z = [  # cmath.phase(p[i]) < cmath.phase(z[i]) < cmath.phase(p[i+1])
-        cmath.rect(1, (cmath.phase(p[i]) + cmath.phase(p[i+1])) / 2)
-        for i in range(len(p) - 1)
-    ]
-    z.append(cmath.rect(
-        abs(p[-1]),
-        (cmath.phase(p[-1]) + math.pi) / 2,
-    ))
-    fil = Filter(p, z, 1, n, [i.amp for i in envelope])
-    e = fil.calc_error()
-    heat = HEAT
-    # loop
-    step = 0
-    while step < STEPS:
-        if step == STEPS-1 and args.mutate_interactive:
-            fil_tries = [copy.deepcopy(fil)]
-            pdb.set_trace()
-        else:
-            fil_tries = [fil]+[fil.mutate(heat, MAX_POLE_ABS) for i in range(BRANCHES)]
-        e_tries = [i.calc_error() for i in fil_tries]
-        i = e_tries.index(min(e_tries))
-        fil = fil_tries[i]
-        e = e_tries[i]
-        print('step', step, 'error:', e)
-        heat *= ANNEALING_MULTIPLIER
-        step += 1
-    if any(i.imag < 0 for i in fil.p) or any(abs(i) > MAX_POLE_ABS for i in fil.p):
-        raise Exception('improper final pole')
-    # normalize
-    fil.k /= math.sqrt(fil.energy())
     #----- tone vs noise -----#
     # autocorrelation
     freq_i = 60
@@ -354,9 +174,7 @@ def parameterize(x):
     noise_amp = math.sqrt(power * (1 - tone))
     #----- outputs -----#
     return {
-        'poles': [json_complex(i.real, i.imag) for i in fil.p],
-        'zeros': [json_complex(i.real, i.imag) for i in fil.z],
-        'gain': fil.k,
+        'formants': formants,
         'tone_amp': tone_amp,
         'noise_amp': noise_amp,
     }
@@ -394,18 +212,6 @@ phonetics = [
     'sh', 'sh_v', 'h', 'f', 'v', 'th', 'th_v', 's', 'z', 'm', 'n', 'ng', 'r', 'l',
     'p', 'b', 't', 'd', 'k', 'g', 'ch', 'j', '0',
 ]
-if args.plot_spectra:
-    grid_w = 8
-    if args.only:
-        grid_w = 1
-    plot = dpc.Plot(
-        transform=dpc.transforms.Compound(
-            dpc.transforms.Grid(4200, 2, grid_w),
-            (dpc.transforms.Default('wby'), 3),
-        ),
-        primitive=dpc.primitives.Line(),
-        hide_axes=True,
-    )
 for i, phonetic in enumerate(phonetics):
     if args.only and phonetic not in args.only:
         continue
@@ -414,47 +220,9 @@ for i, phonetic in enumerate(phonetics):
     print(phonetic)
     if phonetic != '0':
         x = load(args.phonetics_file_path, (i * 10 + 4) * SAMPLE_RATE, 4 * SAMPLE_RATE)
-    if args.plot_spectra:
-        if phonetic == '0':
-            continue
-        asset_path = f'assets/phonetics/{phonetic}.phonetic.json'
-        if os.path.exists(asset_path):
-            with open(asset_path) as f:
-                asset = json.loads(f.read())
-            asset_iter = iter(asset.get('frames', [asset]))
-        else:
-            asset_iter = None
-        cuts = cut_phonetic(x)
-        for j, cut in enumerate(cuts):
-            name = phonetic
-            if len(cuts) != 1:
-                name += str(j)
-            t = plot.transform(-20, 0, 0, plot.series)
-            t.update({'r': 255, 'g': 0, 'b': 255})
-            plot.text(name, **t)
-            xf = fft(cut[:4096])
-            xf = [float(abs(i)) for i in xf[:len(xf)//2+1]]
-            h_max = max(xf)
-            xf = [i / h_max for i in xf]
-            if len(xf) < 4096:
-                plot.plot([i / len(xf) * 2048 for i in range(len(xf))], xf)
-            else:
-                plot.plot(xf)
-            if asset_iter:
-                asset_frame = next(asset_iter)
-                poles = asset_frame['poles']
-                poles = [i['re'] + 1j * i['im'] for i in poles]
-                zeros = asset_frame['zeros']
-                zeros = [i['re'] + 1j * i['im'] for i in zeros]
-                gain = asset_frame['gain']
-                envelope = calc_envelope(cut, calc_n(cut))
-                Filter(poles, zeros, gain, 4096, envelope).plot(scale=1, plot=plot)
-        continue
     if phonetic == '0':
         params = {
-            'poles': [json_complex(0, 0)] * args.order,
-            'zeros': [json_complex(0, 0)] * args.order,
-            'gain': 0,
+            'formants': {0: 0 for i in range(args.order)},
             'tone_amp': 0,
             'noise_amp': 0,
         }
@@ -468,5 +236,3 @@ for i, phonetic in enumerate(phonetics):
     print(params)
     with open(out_file_path, 'w') as file:
         file.write(params)
-if args.plot_spectra:
-    plot.show()
