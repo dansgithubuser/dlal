fde3d07d8999d86c6aaa5f25763758fc560860cd
Dan <dansonlinepresence@gmail.com>
2022-10-13 01:21:43 -0400

    smarter peak

--- components/peak/src/lib.rs
+++ components/peak/src/lib.rs
@@ -1,4 +1,4 @@
-use dlal_component_base::component;
+use dlal_component_base::{component, CmdResult, json, serde_json};
 
 component!(
     {"in": ["audio"], "out": ["audio"]},
@@ -7,14 +7,16 @@ component!(
         "multi",
         "audio",
         "check_audio",
-        {"name": "field_helpers", "fields": ["value"], "kinds": ["r"]},
         {"name": "field_helpers", "fields": ["decay"], "kinds": ["rw", "json"]},
     ],
     {
-        value: f32,
+        lo: f32,
+        hi: f32,
         decay: f32,
     },
-    {},
+    {
+        "value": {"args": []},
+    },
 );
 
 impl ComponentTrait for Component {
@@ -24,15 +26,25 @@ impl ComponentTrait for Component {
 
     fn run(&mut self) {
         for i in &mut self.audio {
-            self.value *= self.decay;
-            if self.value < i.abs() {
-                self.value = i.abs();
+            self.lo *= self.decay;
+            self.hi *= self.decay;
+            if *i < self.lo {
+                self.lo = *i;
             }
-            *i = self.value;
+            if *i > self.hi {
+                self.hi = *i;
+            }
+            *i = self.hi - self.lo;
         }
         self.multi_audio(&self.audio);
         for i in &mut self.audio {
             *i = 0.0;
         }
+    }
+}
+
+impl Component {
+    fn value_cmd(&mut self, _body: serde_json::Value) -> CmdResult {
+        Ok(Some(json!(self.hi - self.lo)))
     }
 }
