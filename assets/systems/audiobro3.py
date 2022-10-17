d7653f1b0d640ab744431e31469b894f85165b4d
Dan <dansonlinepresence@gmail.com>
2022-10-16 21:49:42 -0400

    (audiobro3) half volumes of everything but voice

--- assets/systems/audiobro3.py
+++ assets/systems/audiobro3.py
@@ -47,35 +47,42 @@ tape = dlal.Tape(1 << 17)
 #----- bassoons -----#
 bassoon1.repeat()
 bassoon2.repeat()
-bassoon1.normalize(0.3)
-bassoon2.normalize(0.3)
+bassoon1.normalize(0.15)
+bassoon2.normalize(0.15)
 
 #----- accordion -----#
 accordion1.repeat()
 accordion2.repeat()
-accordion1.normalize(0.1)
-accordion2.normalize(0.1)
+accordion1.normalize(0.05)
+accordion2.normalize(0.05)
 
 #----- drum -----#
 # bass
 drum.load('assets/sounds/drum/bass.wav', 35)
 drum.crop(0, 0.2, 35)
+drum.amplify(0.5, 35)
 drum.load('assets/sounds/drum/bass.wav', 36)
 drum.crop(0, 0.2, 36)
+drum.amplify(0.5, 36)
 # snare
 drum.load('assets/sounds/drum/snare.wav', 38)
 drum.clip(0.5, 38)
+drum.amplify(0.5, 38)
 # hat
 drum.load('assets/sounds/drum/hat.wav', 42)
+drum.amplify(0.5, 42)
 # crash
 drum.load('assets/sounds/drum/crash.wav', 57)
+drum.amplify(0.5, 57)
 # side stick
 drum.load('assets/sounds/drum/side-stick.wav', 37)
 drum.resample(1.5, 37)
 drum.amplify(2, 37)
 drum.crop(0, 0.07, 37)
+drum.amplify(0.5, 37)
 # ride bell
 drum.load('assets/sounds/drum/ride-bell.wav', 55)
+drum.amplify(0.5, 55)
 
 #----- guitar -----#
 guitar_strummer.pattern(
@@ -84,13 +91,14 @@ guitar_strummer.pattern(
     'd u udu u ududu'
     'd u udu u udd'
 )
-guitar.normalize(0.3)
+guitar.normalize(0.15)
 
 #----- shakers -----#
 shaker1.load('assets/sounds/drum/shaker1.wav', 82)
+shaker1.amplify(0.5, 82)
 
 shaker2.load('assets/sounds/drum/shaker2.wav', 82)
-shaker2.amplify(0.5, 82)
+shaker2.amplify(0.25, 82)
 
 #----- liner -----#
 liner.load('assets/midis/audiobro3.mid', immediate=True)
