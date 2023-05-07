e86e19bc7d3c9fef8e7dbf30cf31d0647c243a2b
Dan <dansonlinepresence@gmail.com>
2023-05-07 15:02:54 -0400

    English to IPA with CMU Pronouncing Dictionary

--- systems/phonetic_recognizer/__main__.py
+++ systems/phonetic_recognizer/__main__.py
@@ -1,16 +1,31 @@
+print('===== Setup =====')
+import cmudict
+
 import whisper
 
 import argparse
 import pprint
+import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument('audio_path')
 args = parser.parse_args()
 
+print('===== Load Model =====')
 model = whisper.load_model('base')
+
+print('===== Transcribe =====')
 result = model.transcribe(
     args.audio_path,
     verbose=True,
     word_timestamps=True,
 )
+
+print('===== Results =====')
+print('----- Whisper Output -----')
 pprint.pprint(result)
+print('----- IPA -----')
+for segment in result['segments']:
+    for word in segment['words']:
+        ipa = cmudict.convert(re.sub('[ ,.?]', '', word['word']))
+        print(word['start'], word['end'], word['word'], ipa)
