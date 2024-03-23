41d0ab3b345e42862107ff793cfe2350cb5a6301
dan <dansonlinepresence@gmail.com>
2024-03-23 16:53:28 -0400

    lfo offset

--- components/lfo/src/lib.rs
+++ components/lfo/src/lib.rs
@@ -9,11 +9,12 @@ component!(
         "sample_rate",
         "uni",
         "check_audio",
-        {"name": "field_helpers", "fields": ["freq", "amp"], "kinds": ["rw", "json"]},
+        {"name": "field_helpers", "fields": ["freq", "amp", "offset"], "kinds": ["rw", "json"]},
     ],
     {
         freq: f32,
         amp: f32,
+        offset: f32,
         phase: f32,
     },
     {},
@@ -23,6 +24,7 @@ impl ComponentTrait for Component {
     fn init(&mut self) {
         self.freq = 1.0;
         self.amp = 0.1;
+        self.offset = 0.0;
     }
 
     fn run(&mut self) {
@@ -30,7 +32,7 @@ impl ComponentTrait for Component {
         if let Some(output) = self.output.as_ref() {
             let audio = output.audio(self.run_size).unwrap();
             for i in 0..self.run_size {
-                audio[i] += self.amp * self.phase.sin();
+                audio[i] += self.amp * self.phase.sin() + self.offset;
                 self.phase += step;
             }
         } else {
