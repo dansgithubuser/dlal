836fd573e8be6dad5fe648511b4aea1c093dcc7a
dan <dansonlinepresence@gmail.com>
2024-05-12 21:56:30 -0400

    phonetic vocoder multiple files...
    phonetic vocoder output to file
    i16le_to_flac don't read entire file before writign
    afr read wav

--- components/afr/src/lib.rs
+++ components/afr/src/lib.rs
@@ -1,5 +1,22 @@
 use dlal_component_base::{component, err, json, serde_json, Body, CmdResult};
 
+enum WavSamples {
+    Float(hound::WavIntoSamples<std::io::BufReader<std::fs::File>, f32>),
+    Int16(hound::WavIntoSamples<std::io::BufReader<std::fs::File>, i16>),
+}
+
+enum Reader {
+    None,
+    Flac(claxon::FlacReader<std::fs::File>),
+    Wav(WavSamples, u32),
+}
+
+impl Default for Reader {
+    fn default() -> Self {
+        Reader::None
+    }
+}
+
 component!(
     {"in": [""], "out": ["audio"]},
     [
@@ -14,9 +31,9 @@ component!(
         },
     ],
     {
-        reader: Option<claxon::FlacReader<std::fs::File>>,
+        reader: Reader,
         block: Vec<f32>,
-        block_i: usize,
+        sample_i: usize,
         playing: bool,
     },
     {
@@ -32,32 +49,50 @@ component!(
 
 impl ComponentTrait for Component {
     fn run(&mut self) {
-        let reader = match &mut self.reader {
-            Some(v) => v,
-            None => return,
-        };
         let output = match &self.output {
             Some(v) => v,
             None => return,
         };
         let audio = output.audio(self.run_size).unwrap();
-        for i in 0..self.run_size {
-            if self.block_i == self.block.len() {
-                self.block = match reader.blocks().read_next_or_eof(vec![]) {
-                    Ok(Some(block)) => {
-                        (0..block.duration())
-                            .map(|i| block.sample(0, i) as f32 / 0x8000 as f32)
-                            .collect()
-                    }
-                    _ => {
-                        self.playing = false;
-                        return;
-                    }
+        match &mut self.reader {
+            Reader::None => return,
+            Reader::Flac(reader) => for i in 0..self.run_size {
+                if self.sample_i == self.block.len() {
+                    self.block = match reader.blocks().read_next_or_eof(vec![]) {
+                        Ok(Some(block)) => {
+                            (0..block.duration())
+                                .map(|i| block.sample(0, i) as f32 / 0x8000 as f32)
+                                .collect()
+                        }
+                        _ => {
+                            self.playing = false;
+                            return;
+                        }
+                    };
+                    self.sample_i = 0;
+                }
+                audio[i] += self.block[self.sample_i];
+                self.sample_i += 1;
+            },
+            Reader::Wav(samples, _) => for i in 0..self.run_size {
+                audio[i] += match samples {
+                    WavSamples::Float(samples) => match samples.next() {
+                        Some(Ok(sample)) => sample,
+                        _ => {
+                            self.playing = false;
+                            return;
+                        }
+                    },
+                    WavSamples::Int16(samples) => match samples.next() {
+                        Some(Ok(sample)) => sample as f32 / 32768.0,
+                        _ => {
+                            self.playing = false;
+                            return;
+                        }
+                    },
                 };
-                self.block_i = 0;
+                self.sample_i += 1;
             }
-            audio[i] += self.block[self.block_i];
-            self.block_i += 1;
         }
     }
 }
@@ -65,23 +100,33 @@ impl ComponentTrait for Component {
 impl Component {
     fn open_cmd(&mut self, body: serde_json::Value) -> CmdResult {
         let file_path: String = body.arg(0)?;
-        self.reader = match claxon::FlacReader::open(&file_path) {
-            Ok(v) => Some(v),
-            Err(e) => return Err(err!("{:?}", e).into()),
-        };
+        if file_path.ends_with(".flac") {
+            self.reader = match claxon::FlacReader::open(&file_path) {
+                Ok(v) => Reader::Flac(v),
+                Err(e) => return Err(err!("{:?}", e).into()),
+            };
+        } else if file_path.ends_with(".wav") {
+            let reader = hound::WavReader::open(file_path)?;
+            let duration = reader.duration();
+            let spec = reader.spec();
+            self.reader = match (spec.sample_format, spec.bits_per_sample) {
+                (hound::SampleFormat::Float, 32) => Reader::Wav(WavSamples::Float(reader.into_samples()), duration),
+                (hound::SampleFormat::Int, 16) => Reader::Wav(WavSamples::Int16(reader.into_samples()), duration),
+                _ => return Err(err!("Unhandled wav spec {:?}{}.", spec.sample_format, spec.bits_per_sample).into()),
+            }
+        }
         self.playing = true;
         Ok(None)
     }
 
     fn duration_cmd(&mut self, _body: serde_json::Value) -> CmdResult {
-        let reader = match &self.reader {
-            Some(v) => v,
-            None => return Err(err!("No file opened.").into()),
-        };
-        let samples = match reader.streaminfo().samples {
-            Some(v) => v,
-            None => return Err(err!("No samples.").into()),
-        };
-        Ok(Some(json!(samples)))
+        match &self.reader {
+            Reader::None => return Err(err!("No file opened.").into()),
+            Reader::Flac(reader) => match reader.streaminfo().samples {
+                Some(samples) => Ok(Some(json!(samples))),
+                None => Err(err!("No samples.").into()),
+            },
+            Reader::Wav(_, duration) => Ok(Some(json!(duration))),
+        }
     }
 }
