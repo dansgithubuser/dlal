27732bda0498f88845bcc00d760e8f328a2d7f76
dan <dansonlinepresence@gmail.com>
2024-05-26 02:45:26 -0400

    audiobro4 tweaks

--- assets/systems/audiobro4.py
+++ assets/systems/audiobro4.py
@@ -1,18 +1,20 @@
 import dlal
 
+import argparse
 import random
 import sys
 
-def sys_arg(i):
-    if len(sys.argv) > i:
-        return sys.argv[i]
+parser = argparse.ArgumentParser()
+parser.add_argument('--start', '-s', type=float)
+parser.add_argument('--run-size', type=int)
+args = parser.parse_args()
 
 class Violin(dlal.subsystem.Voices):
     def init(self, name, vol=0):
         dlal.subsystem.Voices.init(
             self,
             ('osc', ['saw'], {'stay_on': True}),
-            vol=0.5,
+            vol=1.0,
             randomize_phase=lambda osc: osc.phase(random.random()),
             name=name,
         )
@@ -20,7 +22,7 @@ class Violin(dlal.subsystem.Voices):
             self,
             {
                 'adsr': ('adsr', [3e-5, 1e-5, 0.5, 5e-5]),
-                'lim': ('lim', [0.25, 0.2]),
+                'lim': ('lim', [0.5, 0.4]),
             },
             name=None,
         )
@@ -44,7 +46,7 @@ class Harp(dlal.subsystem.Subsystem):
         dlal.subsystem.Subsystem.init(
             self,
             {
-                'mgain': ('mgain', [0.05]),
+                'mgain': ('mgain', [0.2]),
                 'digitar': ('digitar', [0.3, 0.998]),
                 'lim': ('lim', [0.25, 0.2]),
                 'buf': 'buf',
@@ -61,7 +63,7 @@ class Harp(dlal.subsystem.Subsystem):
         self.digitar.stay_on(True)
 
 #===== init =====#
-audio = dlal.Audio(driver=True)
+audio = dlal.Audio(driver=True, run_size=args.run_size)
 comm = dlal.Comm()
 liner = dlal.Liner()
 
@@ -77,9 +79,10 @@ lpf1 = dlal.Lpf(freq=200)
 bow_buf = dlal.Buf(name='bow_buf')
 
 lpf2 = dlal.Lpf(freq=800)
-delay1 = dlal.Delay(15000, gain_y=0.4)
-delay2 = dlal.Delay(21000, gain_y=0.2)
-reverb = dlal.Reverb(0.8)
+hpf = dlal.Hpf(freq=40)
+delay1 = dlal.Delay(15000, gain_y=0.2)
+delay2 = dlal.Delay(21000, gain_y=0.1)
+reverb = dlal.Reverb(0.6)
 lim = dlal.Lim(1, 0.9, 0.3)
 buf = dlal.Buf()
 tape = dlal.Tape(1 << 17)
@@ -87,7 +90,8 @@ tape = dlal.Tape(1 << 17)
 #===== commands =====#
 #----- liner -----#
 liner.load('assets/midis/audiobro4.mid', immediate=True)
-liner.advance(float(sys_arg(1) or 0))
+if args.start:
+    liner.advance(args.start)
 
 #===== connect =====#
 dlal.connect(
@@ -106,6 +110,7 @@ dlal.connect(
     ],
     [buf,
         '<+', lpf2,
+        '<+', hpf,
         '<+', delay1,
         '<+', delay2,
         '<+', reverb,
@@ -115,4 +120,4 @@ dlal.connect(
 )
 
 #===== start =====#
-dlal.typical_setup()
+dlal.typical_setup(duration=296)
